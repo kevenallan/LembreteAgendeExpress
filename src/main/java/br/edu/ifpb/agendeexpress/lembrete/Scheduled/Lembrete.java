126ab06d75ee220c76d20639f510367eb7784c2d
kevenallan <kevenallan98@gmail.com>
2021-12-28 10:55:11 -0300

    Alterando hora de execução para 11h

--- src/main/java/br/edu/ifpb/agendeexpress/lembrete/Scheduled/Lembrete.java
+++ src/main/java/br/edu/ifpb/agendeexpress/lembrete/Scheduled/Lembrete.java
@@ -28,7 +28,7 @@ public class Lembrete {
 	private HorarioRepository horarioRepository;
 	
 //	@Scheduled(fixedRate = 10000)
-	@Scheduled(cron = "0 35 10 * * *" )
+	@Scheduled(cron = "0 0 11 * * *" )
 	public void x() {
 		LocalDate data = LocalDate.now();
 		List<Horario> horarioLista = this.horarioRepository.buscarHorarioPorDia(data.getYear(), data.getMonthValue(), data.getDayOfMonth()+1);
