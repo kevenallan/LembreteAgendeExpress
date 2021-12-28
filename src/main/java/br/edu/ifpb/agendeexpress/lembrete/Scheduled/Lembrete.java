3b9e014ed6244fc5d7680b60d7f9c13588b80153
kevenallan <kevenallan98@gmail.com>
2021-12-28 14:52:08 -0300

    Ajustando hora da execução do scheduled para 07h

--- src/main/java/br/edu/ifpb/agendeexpress/lembrete/Scheduled/Lembrete.java
+++ src/main/java/br/edu/ifpb/agendeexpress/lembrete/Scheduled/Lembrete.java
@@ -28,7 +28,7 @@ public class Lembrete {
 	private HorarioRepository horarioRepository;
 	
 //	@Scheduled(fixedRate = 10000)
-	@Scheduled(cron = "0 10 11 * * *" )
+	@Scheduled(cron = "0 0 7 * * *" )
 	public void x() {
 		LocalDate data = LocalDate.now();
 		List<Horario> horarioLista = this.horarioRepository.buscarHorarioPorDia(data.getYear(), data.getMonthValue(), data.getDayOfMonth()+1);
